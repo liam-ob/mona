054a411a95c5c60b4c84d4a55f9f98e651e34f78
liam-ob <l.obrien988@gmail.com>
2022-11-26 12:15:55 +0800

    added some stuff?

--- ramram/home/views.py
+++ ramram/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from instascrape import Instascraper
 from .models import InstaPhoto
+import requests
+
 from django.views.generic import TemplateView
+
 # Create your views here.
 
 
@@ -11,29 +13,6 @@ def index(request):
 
 
 def refresh(request):
-    # ramona = Profile('ramona261', data={'username': 'ramona261'})
-    # ramona.scrape()
-    #
-    # recents = ramona.get_recent_posts()
-    # chris_photos = [post for post in recents if not post.is_video]
-    #
-    # for post in chris_photos:
-    #     fname = post.upload_date.strftime("%Y-%m-%d %Hh%Mm")
-    #     if InstaPhoto.objects.get(name=fname).exists():
-    #         pass
-    #     else:
-    #         # post.download(f"{fname}.png")
-    #         InstaPhoto.objects.create(name=fname, image=post)
-
-    with Instascraper() as insta:
-        posts = insta.profile('ramona261').timeline_posts()
-        for post in posts:
-            fname = post.upload_date.strftime("%Y-%m-%d %Hh%Mm")
-            if InstaPhoto.objects.get(name=fname).exists():
-                pass
-            else:
-                # post.download(f"{fname}.png")
-                InstaPhoto.objects.create(name=fname, image=post)
-
-
+    # ramona page id = 292413182
+    requests.get(//graph.facebook.com/{api-version}/292413182/media)
     index(request)
